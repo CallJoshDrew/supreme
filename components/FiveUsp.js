f9ea7568513ac0964c8d6b9f0ae392ea9b2137e3
JosiahOnline <114333920+CallJoshDrew@users.noreply.github.com>
2023-02-15 19:59:37 +0800

    Update FiveUsp.js
    animations

--- components/FiveUsp.js
+++ components/FiveUsp.js
@@ -10,26 +10,71 @@ import LocalFireDepartmentIcon from "@mui/icons-material/LocalFireDepartment";
 import { Container } from "@mui/system";
 
 import { motion } from "framer-motion";
-import { useInView } from 'react-intersection-observer';
+import { useInView } from "react-intersection-observer";
 import { useAnimation } from "framer-motion";
+import { Grid } from "@mui/material";
 
 export default function FiveUsp() {
-  const {ref,  inView} = useInView();
-  const animation = useAnimation();
+  const { ref, inView } = useInView({
+    threshold: 0.2,
+  });
+  const animation1 = useAnimation();
+  const animation2 = useAnimation();
+  const animation3 = useAnimation();
+  const animation4 = useAnimation();
+  const animation5 = useAnimation();
   useEffect(() => {
-    if(inView){
-      animation.start({
-        x:0,
-        transition:{
-          type:  'spring', duration:2, bounce:0.5
-        }
+    if (inView) {
+      animation1.start({
+        x: 0,
+        transition: {
+          type: "spring",
+          duration: 2,
+          bounce: 0.5,
+        },
+      });
+      animation2.start({
+        x: 0,
+        transition: {
+          type: "spring",
+          duration: 2.5,
+          bounce: 0.5,
+        },
+      });
+      animation3.start({
+        x: 0,
+        transition: {
+          type: "spring",
+          duration: 3,
+          bounce: 0.5,
+        },
+      });
+      animation4.start({
+        x: 0,
+        transition: {
+          type: "spring",
+          duration: 3.5,
+          bounce: 0.5,
+        },
+      });
+      animation5.start({
+        x: 0,
+        transition: {
+          type: "spring",
+          duration: 4,
+          bounce: 0.5,
+        },
       });
     }
-    if(!inView){
-      animation.start({x:'-100vw'})
+    if (!inView) {
+      animation1.start({ x: "-100vw" });
+      animation2.start({ x: "300vw" });
+      animation3.start({ x: "-100vw" });
+      animation4.start({ x: "300vw" });
+      animation5.start({ x: "-100vw" });
     }
-  }, [inView, animation])
-  
+  }, [inView, animation1, animation2, animation3, animation4, animation5]);
+
   return (
     <Container maxWidth="lg">
       <Box
@@ -45,156 +90,163 @@ export default function FiveUsp() {
           // padding: "30px",
         }}>
         <Box ref={ref}>
-          <motion.div animate={animation} >
-            <Card
-              sx={{
-                display: "flex",
-                justifyContent: "center",
-                alignItems: "center",
-                padding: "10px",
-                margin: "20px",
-                boxShadow: 2,
-                border: 0,
-                borderRadius: "20px",
-              }}>
-              <Box
-                sx={{
-                  width: 70,
-                  height: 65,
-                  display: "flex",
-                  justifyContent: "center",
-                  alignItems: "center",
-                }}>
-                <CallIcon sx={{ fontSize: "2.5rem", color: "rgb(255,211,51)" }} />
-              </Box>
-              <Box sx={{ padding: "10px" }}>
-                <Typography variant="h6" component="div">
-                  Call Support
-                </Typography>
-                <Typography variant="body2">Do not be shy, call us anytime, we are ready to serve!</Typography>
-              </Box>
-            </Card>
-            </motion.div>
-        </Box>
-        <Box>
-          <Card
-            sx={{
-              display: "flex",
-              justifyContent: "center",
-              alignItems: "center",
-              margin: "20px",
-              padding: "10px",
-              boxShadow: 2,
-              border: 0,
-              borderRadius: "20px",
-            }}>
-            <Box
-              sx={{
-                width: 70,
-                height: 65,
-                display: "flex",
-                justifyContent: "center",
-                alignItems: "center",
-              }}>
-              <HealthAndSafetyIcon sx={{ fontSize: "2.5rem", color: "rgb(255,211,51)" }} />
-            </Box>
-            <Box sx={{ padding: "10px" }}>
-              <Typography variant="h6" component="div">
-                Safety First
-              </Typography>
-              <Typography variant="body2">Our Working Enviroment are based on ISO11111.</Typography>
-            </Box>
-          </Card>
-        </Box>
-        <Box>
-          <Card
-            sx={{
-              display: "flex",
-              justifyContent: "center",
-              alignItems: "center",
-              margin: "20px",
-              padding: "10px",
-              boxShadow: 2,
-              border: 0,
-              borderRadius: "20px",
-            }}>
-            <Box
-              sx={{
-                width: 70,
-                height: 65,
-                display: "flex",
-                justifyContent: "center",
-                alignItems: "center",
-              }}>
-              <FactoryIcon sx={{ fontSize: "2.5rem", color: "rgb(255,211,51)" }} />
-            </Box>
-            <Box sx={{ padding: "10px" }}>
-              <Typography variant="h5" component="div">
-                Industrial Products
-              </Typography>
-              <Typography variant="body2">All our products are always instock to serve you better!</Typography>
-            </Box>
-          </Card>
-        </Box>
-        <Box>
-          <Card
-            sx={{
-              display: "flex",
-              justifyContent: "center",
-              alignItems: "center",
-              margin: "20px",
-              padding: "10px",
-              boxShadow: 2,
-              border: 0,
-              borderRadius: "20px",
-            }}>
-            <Box
-              sx={{
-                width: 70,
-                height: 65,
-                display: "flex",
-                justifyContent: "center",
-                alignItems: "center",
-              }}>
-              <AssignmentIndIcon sx={{ fontSize: "2.5rem", color: "rgb(255,211,51)" }} />
-            </Box>
-            <Box sx={{ padding: "10px" }}>
-              <Typography variant="h5" component="div">
-                Professional Trained
-              </Typography>
-              <Typography variant="body2">From technicians to administration, our colleagues are all well trained.</Typography>
-            </Box>
-          </Card>
-        </Box>
-        <Box>
-          <Card
-            sx={{
-              display: "flex",
-              justifyContent: "center",
-              alignItems: "center",
-              margin: "20px",
-              padding: "10px",
-              boxShadow: 2,
-              border: 0,
-              borderRadius: "20px",
-            }}>
-            <Box
-              sx={{
-                width: 70,
-                height: 65,
-                display: "flex",
-                justifyContent: "center",
-                alignItems: "center",
-              }}>
-              <LocalFireDepartmentIcon sx={{ fontSize: "2.5rem", color: "rgb(255,211,51)" }} />
-            </Box>
-            <Box sx={{ padding: "10px" }}>
-              <Typography variant="h5" component="div">
-                Great Offer
-              </Typography>
-              <Typography variant="body2">Do not miss out! We always aim to give our best value to all our customers.</Typography>
-            </Box>
-          </Card>
+          <Grid container direction="row" alignItems="center"alignContent='center' align='center' spacing={2.5}>
+            <Grid item xs={12} sm={6} md={6}>
+              <motion.div animate={animation1}>
+                <Card
+                  sx={{
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    padding: "10px",
+                    boxShadow: 2,
+                    border: 0,
+                    borderRadius: "20px",
+                  }}>
+                  <Box
+                    sx={{
+                      width: 70,
+                      height: 65,
+                      display: "flex",
+                      justifyContent: "center",
+                      alignItems: "center",
+                    }}>
+                    <CallIcon sx={{ fontSize: "2.5rem", color: "rgb(255,211,51)" }} />
+                  </Box>
+                  <Box sx={{ padding: "10px" }}>
+                    <Typography variant="h6" component="div">
+                      Call Support
+                    </Typography>
+                    <Typography variant="body2">Do not be shy, call us anytime, we are ready to serve!</Typography>
+                  </Box>
+                </Card>
+              </motion.div>
+            </Grid>
+            <Grid item xs={12} sm={6} md={6}>
+              <motion.div animate={animation2}>
+                <Card
+                  sx={{
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    padding: "10px",
+                    boxShadow: 2,
+                    border: 0,
+                    borderRadius: "20px",
+                  }}>
+                  <Box
+                    sx={{
+                      width: 70,
+                      height: 65,
+                      display: "flex",
+                      justifyContent: "center",
+                      alignItems: "center",
+                    }}>
+                    <HealthAndSafetyIcon sx={{ fontSize: "2.5rem", color: "rgb(255,211,51)" }} />
+                  </Box>
+                  <Box sx={{ padding: "10px" }}>
+                    <Typography variant="h6" component="div">
+                      Safety First
+                    </Typography>
+                    <Typography variant="body2">Our Working Enviroment are based on ISO11111.</Typography>
+                  </Box>
+                </Card>
+              </motion.div>
+            </Grid>
+            <Grid item xs={12} sm={6} md={6}>
+              <motion.div animate={animation3}>
+                <Card
+                  sx={{
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    padding: "10px",
+                    boxShadow: 2,
+                    border: 0,
+                    borderRadius: "20px",
+                  }}>
+                  <Box
+                    sx={{
+                      width: 70,
+                      height: 65,
+                      display: "flex",
+                      justifyContent: "center",
+                      alignItems: "center",
+                    }}>
+                    <FactoryIcon sx={{ fontSize: "2.5rem", color: "rgb(255,211,51)" }} />
+                  </Box>
+                  <Box sx={{ padding: "10px" }}>
+                    <Typography variant="h5" component="div">
+                      Industrial Products
+                    </Typography>
+                    <Typography variant="body2">All products are always instock to serve you better!</Typography>
+                  </Box>
+                </Card>
+              </motion.div>
+            </Grid>
+            <Grid item xs={12} sm={6} md={6}>
+              <motion.div animate={animation4}>
+                <Card
+                  sx={{
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    padding: "10px",
+                    boxShadow: 2,
+                    border: 0,
+                    borderRadius: "20px",
+                  }}>
+                  <Box
+                    sx={{
+                      width: 70,
+                      height: 65,
+                      display: "flex",
+                      justifyContent: "center",
+                      alignItems: "center",
+                    }}>
+                    <AssignmentIndIcon sx={{ fontSize: "2.5rem", color: "rgb(255,211,51)" }} />
+                  </Box>
+                  <Box sx={{ padding: "10px" }}>
+                    <Typography variant="h5" component="div">
+                      Professional Trained
+                    </Typography>
+                    <Typography variant="body2">From technicians to administration, we are all well trained.</Typography>
+                  </Box>
+                </Card>
+              </motion.div>
+            </Grid>
+            <Grid item xs={12} sm={7} md={7}>
+              <motion.div animate={animation5}>
+                <Card
+                  sx={{
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    padding: "10px",
+                    boxShadow: 2,
+                    border: 0,
+                    borderRadius: "20px",
+                  }}>
+                  <Box
+                    sx={{
+                      width: 70,
+                      height: 65,
+                      display: "flex",
+                      justifyContent: "center",
+                      alignItems: "center",
+                    }}>
+                    <LocalFireDepartmentIcon sx={{ fontSize: "2.5rem", color: "rgb(255,211,51)" }} />
+                  </Box>
+                  <Box sx={{ padding: "10px" }}>
+                    <Typography variant="h5" component="div">
+                      Great Offer
+                    </Typography>
+                    <Typography variant="body2">Do not miss out! We always aim to give our best value to all our customers.</Typography>
+                  </Box>
+                </Card>
+              </motion.div>
+            </Grid>
+          </Grid>
         </Box>
       </Box>
     </Container>
